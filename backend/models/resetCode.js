b24892a4205f27b46e5e4664b6b77a0874934911
Indrajeet Singh <indrajeetsinghyadav4@gmail.com>
2023-10-06 13:23:55 +0530

    dcc

--- backend/models/resetCode.js
+++ backend/models/resetCode.js
@@ -2,20 +2,20 @@ import mongoose from "mongoose";
 
 const resetCodeSchema = new mongoose.Schema({
     user_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User', // Reference to the User model
-      required: true,
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User', // Reference to the User model
+        required: true,
     },
     code: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     created_at: {
-      type: Date,
-      default: Date.now,
+        type: Date,
+        default: Date.now,
     },
-  });
-  
-  const ResetCode = mongoose.model('ResetCode', resetCodeSchema);
+});
+
+const ResetCode = mongoose.model('ResetCode', resetCodeSchema);
 
 export default ResetCode
--- backend/models/user.js
+++ backend/models/user.js
@@ -3,27 +3,27 @@ import mongoose from "mongoose";
 
 // Define the User schema
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true, 
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  is_premium: {
-    type: Boolean,
-    default: false, 
-  },
-  total_expense: {
-    type: Number,
-    default: 0, 
-  },
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true, 
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    is_premium: {
+        type: Boolean,
+        default: false, 
+    },
+    total_expense: {
+        type: Number,
+        default: 0, 
+    },
 });
 
 
