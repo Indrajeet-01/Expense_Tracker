38006c8ecb5641a5291f5db88abde43ae9c1b0ff
Indrajeet Singh <indrajeetsinghyadav4@gmail.com>
2023-10-06 12:20:54 +0530

    finish

--- backend/controllers/expense.js
+++ backend/controllers/expense.js
@@ -8,67 +8,65 @@ import AddExpense from "../models/expense.js";
 export const addExpense = async (req, res) => {
     const userId = req.user.id;
     const { amountSpent, expenseDescription, expenseCategory } = req.body;
-  
+
     try {
-      // Create a new AddExpense document
-      const newExpense = new AddExpense({
-        user: userId, // Reference to the user document
-        amount_spent: amountSpent,
-        expense_description: expenseDescription,
-        expense_category: expenseCategory,
-      });
-  
+        const newExpense = new AddExpense({
+            user: userId, 
+            amount_spent: amountSpent,
+            expense_description: expenseDescription,
+            expense_category: expenseCategory,
+        });
+
       // Save the new expense document
-      await newExpense.save();
-  
+        await newExpense.save();
+
       // Update the user's total_expense field
-      const user = await User.findById(userId);
-      if (!user) {
-        return res.status(404).json("User not found");
-      }
-  
-      user.total_expense += amountSpent;
-      await user.save();
-  
-      res.status(201).json(newExpense);
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json("User not found");
+        }
+
+        user.total_expense += amountSpent;
+        await user.save();
+
+        res.status(201).json(newExpense);
     } catch (error) {
-      console.error('Error adding expense:', error);
-      res.status(500).json({ error: 'Failed to add expense.' });
+        console.error('Error adding expense:', error);
+        res.status(500).json({ error: 'Failed to add expense.' });
     }
-  };
-  
+};
+
 // get all expenses
 export const getExpense = async (req, res) => {
     const userId = req.user.id;
-  
+
     try {
-      // Find all expenses for the specified user using the user's ObjectId
-      const expenses = await AddExpense.find({ user: userId });
-  
-      res.status(200).json(expenses);
+        const expenses = await AddExpense.find({ user: userId });
+
+        res.status(200).json(expenses);
     } catch (error) {
-      console.error('Error fetching expenses:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+        console.error('Error fetching expenses:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-  };
+};
 
 // delete expense
 export const deleteExpense = async (req, res) => {
     const userId = req.user.id;
     const expenseId = req.params.expenseId;
-  
+
     try {
       // Find and delete the expense by its _id and associated user
-      const deletedExpense = await AddExpense.findOneAndDelete({ _id: expenseId, user: userId });
-  
-      if (!deletedExpense) {
-        return res.status(404).json({ error: 'Expense not found or you do not have permission to delete it.' });
-      }
-  
-      res.status(200).json({ message: 'Expense deleted successfully.' });
+        const deletedExpense = await AddExpense.findOneAndDelete({ _id: expenseId, user: userId });
+
+        if (!deletedExpense) {
+            return res.status(404).json({ error: 'Expense not found or you do not have permission to delete it.' });
+        }
+
+        res.status(200).json({ message: 'Expense deleted successfully.' });
     } catch (error) {
-      console.error('Error deleting expense:', error);
-      res.status(500).json({ error: 'Failed to delete expense.' });
+        console.error('Error deleting expense:', error);
+        res.status(500).json({ error: 'Failed to delete expense.' });
     }
-  };
+};
 
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -15,182 +15,178 @@ import ResetCode from "../models/resetCode.js";
 export const register = (req, res) => {
     // Check if a user with the given email already exists
     User.findOne({ $or: [{ name: req.body.name }, { email: req.body.email }] })
-      .then(existingUser => {
-        if (existingUser) {
-          return res.status(409).json("User already exists!");
-        }
-  
+        .then(existingUser => {
+            if (existingUser) {
+                return res.status(409).json("User already exists!");
+            }
+
         // Hash the password
         const salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(req.body.password, salt);
-  
-        // Create a new user document
+
         const newUser = new User({
-          name: req.body.name,
-          email: req.body.email,
-          password: hash,
-          is_premium: req.body.is_premium || false, // Set is_premium if provided, default to false
-          total_expense: req.body.total_expense || 0, // Set total_expense if provided, default to 0
+            name: req.body.name,
+            email: req.body.email,
+            password: hash,
+            is_premium: req.body.is_premium || false, 
+            total_expense: req.body.total_expense || 0, 
         });
-  
+
         // Save the new user to the database
         newUser.save()
-          .then(() => {
-            return res.status(200).json("User registered successfully");
-          })
-          .catch(error => {
-            return res.status(500).json(error.message);
-          });
-      })
-      .catch(err => {
-        return res.status(500).json(err.message);
-      });
-  };
+            .then(() => {
+                return res.status(200).json("User registered successfully");
+            })
+            .catch(error => {
+                return res.status(500).json(error.message);
+            });
+        })
+        .catch(err => {
+            return res.status(500).json(err.message);
+    });
+};
+
 // login user
 export const login = (req, res) => {
-    // Find the user by name (assuming name is unique)
+    
     User.findOne({ name: req.body.name })
-      .then(user => {
-        if (!user) {
-          return res.status(404).json("User not found");
-        }
-  
+        .then(user => {
+            if (!user) {
+                return res.status(404).json("User not found");
+            }
+
         // Check if the provided password matches the hashed password in the database
         const isPasswordCorrect = bcrypt.compareSync(req.body.password, user.password);
-  
+
         if (!isPasswordCorrect) {
-          return res.status(400).json("Wrong username or password");
+            return res.status(400).json("Wrong username or password");
         }
-  
+
         // Generate a JSON Web Token (JWT) for user authentication
         const token = jwt.sign({ id: user._id }, "jwtkey");
-  
+
         const responseData = {
-          id: user._id,
-          name: user.name,
-          email: user.email,
-          is_premium: user.is_premium,
-          access_token: token,
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            is_premium: user.is_premium,
+            access_token: token,
         };
-  
+
         // Set the access_token as an HTTP-only cookie for secure storage
         res.cookie("access_token", token, {
-          httpOnly: true,
+            httpOnly: true,
         }).status(200).json(responseData);
-      })
-      .catch(err => {
+    })
+    .catch(err => {
         return res.status(500).json(err.message);
-      });
-  };
+    });
+};
 
 // get leader board by premium member
 export const getLeaderboard = async (req, res) => {
     const userId = req.user.id;
-  
+
     try {
-      // Find the user by their _id and project the 'is_premium' field
-      const user = await User.findById(userId, 'is_premium');
-  
-      if (!user) {
-        return res.status(404).json({ error: 'User not found.' });
-      }
-  
-      const isPremiumMember = user.is_premium;
-  
-      if (!isPremiumMember) {
-        return res.status(403).json({ error: 'Access denied. You must be a premium member to view the leaderboard.' });
-      }
-  
+        const user = await User.findById(userId, 'is_premium');
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found.' });
+        }
+
+        const isPremiumMember = user.is_premium;
+
+        if (!isPremiumMember) {
+            return res.status(403).json({ error: 'Access denied. You must be a premium member to view the leaderboard.' });
+        }
+
       // Fetch users ordered by total_expense in descending order
-      const leaderboard = await User.find({}, 'name total_expense')
-        .sort({ total_expense: -1 });
-  
-      res.status(200).json(leaderboard);
+        const leaderboard = await User.find({}, 'name total_expense')
+            .sort({ total_expense: -1 });
+
+        res.status(200).json(leaderboard);
     } catch (error) {
-      console.error('Error fetching leaderboard data:', error);
-      res.status(500).json({ error: 'Failed to fetch leaderboard data.' });
-    }
-  };
+            console.error('Error fetching leaderboard data:', error);
+            res.status(500).json({ error: 'Failed to fetch leaderboard data.' });
+        }
+};
 
 
 // report generation
 export const getReport = async (req, res) => {
     const userId = req.user.id;
-  
+
     try {
       // Find the user by their _id and project the 'is_premium' field
-      const user = await User.findById(userId, 'is_premium');
-  
-      if (!user) {
-        return res.status(404).json({ error: 'User not found.' });
-      }
-  
-      const isPremiumMember = user.is_premium;
-  
-      if (!isPremiumMember) {
-        return res.status(403).json({ error: 'Access denied. You must be a premium member to generate reports.' });
-      }
-  
+        const user = await User.findById(userId, 'is_premium');
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found.' });
+        }
+
+        const isPremiumMember = user.is_premium;
+
+        if (!isPremiumMember) {
+            return res.status(403).json({ error: 'Access denied. You must be a premium member to generate reports.' });
+        }
+
       // Fetch all expenses for the specific user
-      const expenses = await AddExpense.find({ user: userId });
-  
-      if (expenses.length === 0) {
-        return res.status(404).json({ error: 'No expenses found for the user.' });
-      }
-  
-      // Generate and send the CSV report
-      generateCSVReport(res, expenses);
+        const expenses = await AddExpense.find({ user: userId });
+
+        if (expenses.length === 0) {
+            return res.status(404).json({ error: 'No expenses found for the user.' });
+        }
+
+        generateCSVReport(res, expenses);
     } catch (error) {
-      console.error('Error fetching user premium status or expenses:', error);
-      res.status(500).json({ error: 'Failed to fetch user premium status or expenses.' });
+        console.error('Error fetching user premium status or expenses:', error);
+        res.status(500).json({ error: 'Failed to fetch user premium status or expenses.' });
     }
-  };
-  
-  // Generate CSV report
-  function generateCSVReport(res, expenses) {
+};
+
+// Generate CSV report
+function generateCSVReport(res, expenses) {
     const csvWriter = createCsvWriter({
-      path: 'expense_report.csv',
-      header: [
+        path: 'expense_report.csv',
+        header: [
         
         { id: 'amount_spent', title: 'Amount Spent' },
         { id: 'expense_description', title: 'Expense Description' },
         { id: 'expense_category', title: 'Expense Category' },
-      ],
+        ],
     });
-  
+
     // Write CSV data
     csvWriter.writeRecords(expenses)
-      .then(() => {
+        .then(() => {
         res.setHeader('Content-Type', 'text/csv');
         res.setHeader('Content-Disposition', 'attachment; filename="expense_report.csv"');
-  
+
         // Stream the CSV file to the response
         const fileStream = fs.createReadStream('expense_report.csv');
         fileStream.pipe(res);
-      });
-  }
+    });
+}
 
 // check if user is premium or not
 export const isUserPremium = async (req, res) => {
     const userId = req.user.id;
-  
+
     try {
-      // Find the user by their _id and project only the 'is_premium' field
-      const user = await User.findById(userId, 'is_premium');
-  
-      if (!user) {
-        return res.status(404).json({ error: 'User not found.' });
-      }
-  
-      const isPremium = user.is_premium;
-  
-      // Return the isPremium value in the response
-      res.status(200).json({ isPremium });
+        const user = await User.findById(userId, 'is_premium');
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found.' });
+        }
+
+        const isPremium = user.is_premium;
+
+        res.status(200).json({ isPremium });
     } catch (error) {
-      console.error('Error fetching user premium status:', error);
-      res.status(500).json({ error: 'Failed to fetch user premium status.' });
+        console.error('Error fetching user premium status:', error);
+        res.status(500).json({ error: 'Failed to fetch user premium status.' });
     }
-  };
+};
 
 // logout user
 export const logout = (req,res)=>{
